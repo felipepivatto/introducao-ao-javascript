290db976ba1c83cfc26df1432563f86c7cebed7a
felipe_silva <felipe_silva47@estudante.sesisenai.org.br>
2025-03-25 08:44:21 -0300

    ADD - exercicios 4 5 6 7 8 9 10

--- script.js
+++ script.js
@@ -25,3 +25,42 @@ let original = "JavaScript";
 let invertido = original.split("").reverse().join("");
 console.log(invertido);
 
+function contaCaracter(palavra) {
+    console.log(palavra.length);
+}
+
+contaCaracter("Professor");
+
+let carro = {
+    marca: "BMW",
+    modelo: "320i",
+    ano: 2020,
+}
+console.log(carro.modelo)
+
+function mensagemPersonalizada(nome){
+    console.log("olá, " + nome);
+}
+mensagemPersonalizada(prompt("Digite seu nome"));
+
+function media(n1,n2,n3){
+    console.log( (n1 + n2 + n3)/3);
+}
+media(10,20,30)
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0) {
+        console.log(i + " - Múltiplo de 3");
+    } else {
+        console.log(i + " - Não múltiplo de 3");
+    }
+}
+
+function verificarPalindromo(palavra) {
+    palavra = palavra.toLowerCase();
+    let palavraInvertida = palavra.split('').reverse().join('');
+    return palavra === palavraInvertida;
+}
+
+console.log(verificarPalindromo("arara")); 
+console.log(verificarPalindromo("banana"));
